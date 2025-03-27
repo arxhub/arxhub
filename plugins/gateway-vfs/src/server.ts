af26c71efa9b0ef007ffe3e7a0d79db1162c5f72
rougsig <rougsig@gmail.com>
2025-03-27 18:46:21 +0400

    feat(gateway-vfs): Merge plugin into server file

--- plugins/gateway-vfs/src/server.ts
+++ plugins/gateway-vfs/src/server.ts
@@ -1,2 +1,28 @@
-import PluginInstance from './plugin'
-export default PluginInstance
+import { type ArxHub, Plugin } from '@arxhub/core'
+import { GatewayExtension } from '@arxhub/plugin-gateway/api'
+import { VirtualFileSystemExtension } from '@arxhub/plugin-vfs/api'
+import { files } from './routes/files'
+import type { FilesEnv } from './types'
+
+export class GatewayVFSPlugin extends Plugin<ArxHub> {
+  constructor() {
+    super({
+      name: GatewayVFSPlugin.name,
+      version: '0.1.0',
+      author: '',
+    })
+  }
+
+  async configure(target: ArxHub): Promise<void> {
+    const { vfs } = target.extensions.getByType(VirtualFileSystemExtension)
+    const { gateway } = target.extensions.getByType(GatewayExtension)
+
+    gateway.use<string, FilesEnv>('/vfs/*', async (ctx, next) => {
+      ctx.set('vfs', vfs)
+      await next()
+    })
+    gateway.route('/vfs/files', files)
+  }
+}
+
+export default new GatewayVFSPlugin()
--- plugins/gateway-vfs/vite.config.ts
+++ plugins/gateway-vfs/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 
 // biome-ignore format: Manual formatting is more readable
 export default defineConfig((env) => createNodeConfig(__dirname, env, [
-	'src/api.ts',
 	'src/client.ts',
 	'src/server.ts',
 ]))
