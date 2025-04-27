7e5efb312e142ef0711695191802aa2de01095a9
rougsig <rougsig@gmail.com>
2025-04-27 13:11:04 +0400

    feat(vfs): Moving from pouchdb to signaldb

--- packages/vfs/src/searchable-file-system.ts
+++
@@ -1,7 +0,0 @@
-import type { Mango } from './mango'
-import type { VirtualFile, VirtualFileProps } from './virtual-file'
-import type { VirtualFileSystem } from './virtual-file-system'
-
-export interface SearchableFileSystem extends VirtualFileSystem {
-  select<T extends VirtualFileProps>(query: Mango.SelectQuery<T>): Promise<VirtualFile[]>
-}
