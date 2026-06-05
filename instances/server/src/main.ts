b78444a96acb81152ef31f3957939ca98ee1ad41
rougsig <rougsig@gmail.com>
2026-06-05 17:39:05 +0200

    refactor(server): extract arxhub factory and replace tsx with Vite dev plugin for HMR

--- instances/server/src/main.ts
+++ instances/server/src/main.ts
@@ -1,17 +1,6 @@
-import { homedir } from 'node:os'
-import { join } from 'node:path'
-import { ArxHub } from '@arxhub/core'
-import GatewayServerPlugin from '@arxhub/plugin-gateway/server'
-import { VfsHttpServerPlugin } from '@arxhub/vfs-http/server'
-import { NodeFileSystem } from '@arxhub/vfs-node'
+import { createArxHub } from './arxhub'
 
-const arxhub = new ArxHub()
-const vfs = new NodeFileSystem(join(homedir(), '.arxhub'), arxhub.logger)
-
-arxhub.plugins.register(GatewayServerPlugin)
-arxhub.plugins.register(VfsHttpServerPlugin, () => ({ vfs }))
-
-await arxhub.start()
+const arxhub = await createArxHub()
 
 console.log('ArxHub server started')
 
