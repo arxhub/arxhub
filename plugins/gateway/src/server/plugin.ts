8340572ee0fd5bcda583a3812aa4c4e1d6afb3ae
rougsig <rougsig@gmail.com>
2026-06-05 17:38:59 +0200

    feat(gateway): expose port property and support configurable port in plugin

--- plugins/gateway/src/server/plugin.ts
+++ plugins/gateway/src/server/plugin.ts
@@ -4,8 +4,11 @@ import { GatewayServerExtension } from './extension'
 import { healthcheckRoute } from './routes/healthcheck'
 
 export class GatewayServerPlugin extends Plugin<ArxHub> {
-  constructor(args: PluginArgs) {
-    super(args, manifest)
+  private readonly port: number
+
+  constructor({ port = 3000, ...args }: PluginArgs & { port?: number }) {
+    super(args as PluginArgs, manifest)
+    this.port = port
   }
 
   override create(target: ArxHub): void {
@@ -19,7 +22,7 @@ export class GatewayServerPlugin extends Plugin<ArxHub> {
 
   override start(target: ArxHub): Promise<void> {
     const { gateway } = target.extensions.get(GatewayServerExtension)
-    return gateway.listen()
+    return gateway.listen(this.port)
   }
 
   override stop(target: ArxHub): Promise<void> {
